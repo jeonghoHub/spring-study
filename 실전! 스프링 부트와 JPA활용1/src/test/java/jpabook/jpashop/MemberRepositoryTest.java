7378e8057d5831ed8c2da79f25b57e364756631e
jeonghoHub <tjsrsdqq4@gmail.com>
2021-12-17 18:01:58 +0900

    123

--- 실전! 스프링 부트와 JPA활용1/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ 실전! 스프링 부트와 JPA활용1/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,7 +22,7 @@ public class MemberRepositoryTest {
     public void testMember() throws Exception {
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setUsername("member123");
 
         //when
         Long savedId = memberRepository.save(member);
@@ -30,5 +31,6 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
