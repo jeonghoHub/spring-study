fdcadd6a03e4310756c8e07eeacd70bb89f8a5b9
jeonghoHub <tjsrsdqq4@gmail.com>
2021-12-21 13:54:25 +0900

    회원 도메인  및 상품 도메인 개발완료

--- 실전! 스프링 부트와 JPA활용1/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 실전! 스프링 부트와 JPA활용1/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,6 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
     @Id
     @GeneratedValue
@@ -20,12 +20,31 @@ public abstract class Item {
     private Long id;
 
     private String name;
-
     private int price;
-
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categoryies = new ArrayList<>();
 
+    //비즈니스 로직
+
+    /**
+     * 
+     * stock증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *
+     * stock감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
