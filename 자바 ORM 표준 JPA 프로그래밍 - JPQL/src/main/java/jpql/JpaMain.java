312a67743efcc7fe85dafd3475bb618e30ad603b
jeonghoHub <tjsrsdqq4@gmail.com>
2022-02-23 17:08:38 +0900

    있나?

--- 자바 ORM 표준 JPA 프로그래밍 - JPQL/src/main/java/jpql/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - JPQL/src/main/java/jpql/JpaMain.java
@@ -16,29 +16,46 @@ public class JpaMain {
 
         try{
             Team team = new Team();
-            team.setName("teamA");
+            team.setName("팀A");
             em.persist(team);
 
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
             Member member = new Member();
-            member.setUsername(null);
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
+            member.setUsername("회원1");
             member.setTeam(team);
-
             em.persist(member);
+
+            Member member1 = new Member();
+            member1.setUsername("회원2");
+            member1.setTeam(team);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원3");
+            member2.setTeam(teamB);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username from Team t join t.members m";
+            String query = "select m from Member m join fetch m.team";
 
-            List<String> resultList = em.createQuery(query, String.class)
+            List<Member> resultList = em.createQuery(query, Member.class)
                     .getResultList();
 
-            for (String s : resultList) {
-                System.out.println("s = " + s);
+            for (Member member3 : resultList) {
+                System.out.println("member = " + member3.getUsername() + "| team = " + member3.getTeam().getName());
             }
+
+//            for (Team team1 : resultList) {
+//                System.out.println("team = " + team1.getName() + "|" + team1.getMembers().size());
+//                for(Member member4 : team1.getMembers()) {
+//                    System.out.println("-> member = " + member4);
+//                }
+//            }
 
             tx.commit();
         } catch(Exception e) {
